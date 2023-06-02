cdcd5d895ecadc0a01436778dedb299527cba976
Cloves Neto <cvr.neto20@gmail.com>
2023-06-02 14:13:03 -0300

    att devneto

--- script.js
+++ script.js
@@ -7,14 +7,25 @@ document.addEventListener("DOMContentLoaded",  () => {
         }).type('Web Developer', {delay: 900}).delete(13)
         .type('Ui/Ux Designer', {delay: 900})
         .go();
-  });
-
+});
 
   // Animação menu-mobile ( Inserir Classe para estilizar)
-
-  function ativar(){
+function ativar(){
     const menuMobile = document.getElementById('moba');
     menuMobile.addEventListener('click', ()=>{
         menuMobile.classList.toggle('menu-ativo');
     })  
 }
+
+// Style referencia de sessão do menu
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav ul li');
+
+function menuAtivo(){
+    let len = sections.length;
+    while(--len && window.scrollY + 97 < sections[len].offsetTop){}
+    navLinks.forEach(nvl => nvl.classList.remove('ativo'));
+    navLinks[len].classList.add('ativo');
+}
+menuAtivo();
+window.addEventListener("scroll", menuAtivo);
